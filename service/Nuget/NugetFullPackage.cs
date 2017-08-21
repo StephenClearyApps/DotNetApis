4affbb5c35ef344cf67ce31674a6bf87d244bfa0
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-08-21 12:32:39 -0400

    More package helper types.

---
+++ service/Nuget/NugetFullPackage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuget
+{
+    /// <summary>
+    /// An in-memory nupkg along with its external metadata.
+    /// </summary>
+    public sealed class NugetFullPackage
+    {
+        public NugetFullPackage(NugetPackage package, NugetPackageExternalMetadata externalMetadata)
+        {
+            Package = package ?? throw new ArgumentNullException(nameof(package));
+            ExternalMetadata = externalMetadata ?? throw new ArgumentNullException(nameof(externalMetadata));
+        }
+
+        /// <summary>
+        /// The in-memory package.
+        /// </summary>
+        public NugetPackage Package { get; }
+
+        /// <summary>
+        /// The external metadata for the package.
+        /// </summary>
+        public NugetPackageExternalMetadata ExternalMetadata { get; }
+    }
+}
---
+++ service/Nuget/NugetPackageExternalMetadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuget
+{
+    /// <summary>
+    /// Package metadata that exists outside the package itself.
+    /// </summary>
+    public sealed class NugetPackageExternalMetadata
+    {
+        public NugetPackageExternalMetadata(DateTimeOffset published)
+        {
+            Published = published;
+        }
+
+        /// <summary>
+        /// When the package was published.
+        /// </summary>
+        public DateTimeOffset Published { get; }
+    }
+}
