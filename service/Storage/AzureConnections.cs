5569c278f48adb737d67481d90eed630bb8f9492
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-08-21 10:33:01 -0400

    Add Storage project.

---
+++ service/Storage/AzureConnections.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Common;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Storage
+{
+    public sealed class AzureConnections
+    {
+        public AzureConnections()
+        {
+            CloudStorageAccount = CloudStorageAccount.Parse(Config.GetSetting("StorageConnectionString"));
+            CloudBlobClient = CloudStorageAccount.CreateCloudBlobClient();
+        }
+
+        public CloudStorageAccount CloudStorageAccount { get; }
+        public CloudBlobClient CloudBlobClient { get; }
+    }
+}
