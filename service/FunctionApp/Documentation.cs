253c871f5b9304959ca754268428f8105835511c
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-08-22 13:03:21 -0400

    fix: report container issues as unexpected exceptions

--- service/FunctionApp/Documentation.cs
+++ service/FunctionApp/Documentation.cs
@@ -19,44 +19,47 @@ namespace FunctionApp
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "0/doc")]HttpRequestMessage req, TraceWriter log)
         {
             Defaults.ApplyRequestHandlingDefaults(req);
-            AmbientContext.Initialize(Enumerables.Return<ILogger>(new InMemoryLogger(), new TraceWriterLogger(log)));
-
-            using (AsyncScopedLifestyle.BeginScope(GlobalConfig.Container))
+            try
             {
-                var logger = GlobalConfig.Container.GetInstance<ILogger>();
-                try
+                AmbientContext.Initialize(Enumerables.Return<ILogger>(new InMemoryLogger(), new TraceWriterLogger(log)));
+
+                using (AsyncScopedLifestyle.BeginScope(GlobalConfig.Container))
                 {
-                    GlobalConfig.EnsureInitilizationComplete();
-
-                    var query = req.GetQueryNameValuePairs().ToList();
-                    var jsonVersion = query.Required("jsonVersion", int.Parse);
-                    var packageId = query.Required("packageId");
-                    var packageVersion = query.Optional("packageVersion");
-                    var targetFramework = query.Optional("targetFramework");
-                    logger.Trace($"Received request for jsonVersion={jsonVersion}, packageId=`{packageId}`, packageVersion=`{packageVersion}`, targetFramework=`{targetFramework}`");
-
-                    if (jsonVersion < JsonFactory.Version)
+                    var logger = GlobalConfig.Container.GetInstance<ILogger>();
+                    try
                     {
-                        logger.Trace($"Requested JSON version {jsonVersion} is old; current JSON version is {JsonFactory.Version}; returning 422");
-                        return req.CreateResponse((HttpStatusCode) 422, "Application needs to update; refresh the page.");
+                        GlobalConfig.EnsureInitilizationComplete();
+
+                        var query = req.GetQueryNameValuePairs().ToList();
+                        var jsonVersion = query.Required("jsonVersion", int.Parse);
+                        var packageId = query.Required("packageId");
+                        var packageVersion = query.Optional("packageVersion");
+                        var targetFramework = query.Optional("targetFramework");
+                        logger.Trace($"Received request for jsonVersion={jsonVersion}, packageId=`{packageId}`, packageVersion=`{packageVersion}`, targetFramework=`{targetFramework}`");
+
+                        if (jsonVersion < JsonFactory.Version)
+                        {
+                            logger.Trace($"Requested JSON version {jsonVersion} is old; current JSON version is {JsonFactory.Version}; returning 422");
+                            return req.CreateResponse((HttpStatusCode)422, "Application needs to update; refresh the page.");
+                        }
+
+                        var handler = GlobalConfig.Container.GetInstance<DocRequestHandler>();
+                        var result = await handler.GetDocAsync(packageId, packageVersion, targetFramework);
+
+                        logger.Trace($"Success!");
+                        return req.CreateResponse(HttpStatusCode.OK, "Hello " + result);
                     }
-
-                    var handler = GlobalConfig.Container.GetInstance<DocRequestHandler>();
-                    var result = await handler.GetDocAsync(packageId, packageVersion, targetFramework);
-
-                    logger.Trace($"Success!");
-                    return req.CreateResponse(HttpStatusCode.OK, "Hello " + result);
+                    catch (ExpectedException ex)
+                    {
+                        logger.Trace($"Returning {(int)ex.HttpStatusCode}: {ex.Message}");
+                        return req.CreateErrorResponseWithLog(ex);
+                    }
                 }
-                catch (ExpectedException ex)
-                {
-                    logger.Trace($"Returning {(int) ex.HttpStatusCode}: {ex.Message}");
-                    return req.CreateErrorResponseWithLog(ex);
-                }
-                catch (Exception ex)
-                {
-                    logger.Trace($"Returning 500: {ex.Message}");
-                    return req.CreateErrorResponseWithLog(ex);
-                }
+            }
+            catch (Exception ex)
+            {
+                log.Error($"Returning 500: {ex.Message}");
+                return req.CreateErrorResponseWithLog(ex);
             }
         }
     }
