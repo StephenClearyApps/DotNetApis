77b63f5cb4e54162174fe9b2b37dd2aab2c0974d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-09-03 21:16:06 -0400

    add XmldocFormatter

---
+++ service/DotNetApis.Logic/Formatting/XmldocFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetApis.Logic.Formatting
+{
+    public sealed class XmldocFormatter
+    {
+        private readonly ILogger _logger;
+
+        public XmldocFormatter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
