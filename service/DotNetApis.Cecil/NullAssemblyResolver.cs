69f14467d20666f69997c93ae57b0d60b6c6dc8a
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-09-04 16:58:56 -0400

    add NullAssemblyResolver

---
+++ service/DotNetApis.Cecil/NullAssemblyResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace DotNetApis.Cecil
+{
+    /// <summary>
+    /// An assembly resolver that never resolves assemblies.
+    /// </summary>
+    public sealed class NullAssemblyResolver : AssemblyResolverBase
+    {
+        public override AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters) => null;
+    }
+}
