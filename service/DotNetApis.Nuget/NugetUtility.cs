93fdabf961b8eaa512205c9b8d50872816f41e60
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-09-05 09:45:17 -0400

    normalize PlatformTarget FrameworkNames to preferred equivalents
    E.g., prefer win81 over netcore451

--- service/DotNetApis.Nuget/NugetUtility.cs
+++ service/DotNetApis.Nuget/NugetUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Versioning;
+﻿using System.Linq;
+using System.Runtime.Versioning;
 using NuGet.Frameworks;
 
 namespace DotNetApis.Nuget
@@ -12,11 +13,28 @@ namespace DotNetApis.Nuget
         public static bool IsFrameworkPortable(this FrameworkName frameworkName) => NuGetFramework.ParseFrameworkName(frameworkName.FullName, DefaultFrameworkNameProvider.Instance).IsPCL;
 
         /// <summary>
-        /// Normalizes the framework name into a full framework name.
+        /// Normalizes the framework name, extending it into a full framework name if necessary, and choosing the most desirable equivalent (e.g., `win81` instead of `netcore451`).
         /// </summary>
         /// <param name="frameworkName">The framework name.</param>
-        public static FrameworkName NormalizeFrameworkName(this FrameworkName frameworkName) => 
-            new FrameworkName(NuGetFramework.ParseFrameworkName(frameworkName.FullName, DefaultFrameworkNameProvider.Instance).DotNetFrameworkName);
+        public static FrameworkName NormalizeFrameworkName(this FrameworkName frameworkName)
+        {
+            var provider = DefaultFrameworkNameProvider.Instance;
+
+            // Noramlize into a full framework name.
+            var result = NuGetFramework.ParseFrameworkName(frameworkName.FullName, provider);
+
+            // Choose the most desirable form of the framework name.
+            if (provider.TryGetEquivalentFrameworks(result, out var options))
+            {
+                foreach (var option in options)
+                {
+                    if (provider.CompareEquivalentFrameworks(result, option) > 0)
+                        result = option;
+                }
+            }
+
+            return new FrameworkName(result.DotNetFrameworkName);
+        }
 
         /// <summary>
         /// Gets the short framework name.
