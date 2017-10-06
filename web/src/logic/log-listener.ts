ceb7d4d10de369dbe0593717d277ea05dce45367
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-10-06 19:41:06 -0400

    remove Ably history
    It almost never returns results, and it can throw errors.

--- web/src/logic/log-listener.ts
+++ web/src/logic/log-listener.ts
@@ -6,14 +6,6 @@ function attachAsync(channel: Ably.ablyLib.RealtimeChannel): Promise<void> {
     return new Promise((resolve, reject) => channel.attach(err => err ? reject(err) : resolve()));
 }
 
-function historyAsync(channel: Ably.ablyLib.RealtimeChannel, params: Ably.ablyLib.RealtimePresenceHistoryParams): Promise<Ably.ablyLib.PaginatedResult<Ably.ablyLib.Message>> {
-    return new Promise((resolve, reject) => channel.history(params, (err, page) => err ? reject(err) : resolve(page)));
-}
-
-function nextAsync<T>(page: Ably.ablyLib.PaginatedResult<T>): Promise<Ably.ablyLib.PaginatedResult<T>> {
-    return new Promise((resolve, reject) => page.next((err, nextPage) => err ? reject(err) : resolve(nextPage)));
-}
-
 export class LogListener {
     private channel: Ably.ablyLib.RealtimeChannel;
     constructor(name: string, private handler: (err: Ably.ablyLib.ErrorInfo | Error, message: Ably.ablyLib.Message, meta: string) => void) {
@@ -23,21 +15,8 @@ export class LogListener {
     public async listen() {
         try {
             this.handler(undefined, undefined, "Establishing connection to log streaming service...");
-            const buffer: Ably.ablyLib.Message[] = [];
             await attachAsync(this.channel);
-            this.handler(undefined, undefined, "Connection established; reviewing history...");
-            let page = await historyAsync(this.channel, { untilAttach: true, direction: "forwards" });
-            while (true) {
-                buffer.push(...page.items);
-                if (page.isLast()) {
-                    break;
-                }
-                page = await nextAsync(page);
-            }
-            this.handler(undefined, undefined, "Switching to live updates...");
-            for (let message of buffer) {
-                this.handler(undefined, message, undefined);
-            }
+            this.handler(undefined, undefined, "Connection established; subscribing to live updates...");
             this.channel.subscribe(message => {
                 this.handler(undefined, message, undefined);
             });
