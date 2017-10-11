aec5c3ce4e029045212be1e79cae287f68fdc383
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-10-10 23:22:14 -0400

    Add missing JSON property names.

--- service/DotNetApis.Structure/Xmldoc/Xmldoc.cs
+++ service/DotNetApis.Structure/Xmldoc/Xmldoc.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace DotNetApis.Structure.Xmldoc
 {
@@ -10,31 +11,37 @@ namespace DotNetApis.Structure.Xmldoc
         /// <summary>
         /// The <c>summary</c> or <c>value</c> documentation.
         /// </summary>
+        [JsonProperty("b")]
         public IXmldocNode Basic { get; set; }
 
         /// <summary>
         /// The <c>remarks</c> documentation.
         /// </summary>
+        [JsonProperty("m")]
         public IXmldocNode Remarks { get; set; }
 
         /// <summary>
         /// The <c>example</c> documentation.
         /// </summary>
+        [JsonProperty("e")]
         public IReadOnlyList<IXmldocNode> Examples { get; set; }
 
         /// <summary>
         /// The <c>seealso</c> documentation.
         /// </summary>
+        [JsonProperty("s")]
         public IReadOnlyList<IXmldocNode> SeeAlso { get; set; }
 
         /// <summary>
         /// The <c>exception</c> documentation.
         /// </summary>
+        [JsonProperty("x")]
         public IReadOnlyList<IXmldocNode> Exceptions { get; set; }
 
         /// <summary>
         /// The <c>returns</c> documentation.
         /// </summary>
+        [JsonProperty("r")]
         public IXmldocNode Returns { get; set; }
 
         public override string ToString() => Basic?.ToString() ?? "";
