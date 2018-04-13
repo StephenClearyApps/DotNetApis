ba73b51bce6bced59f60631e73a19c713bb25e7d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-13 08:11:40 -0400

    Update safe NuGet packages.

--- service/DotNetApis.SimpleInjector/ContainerExtensions.cs
+++ service/DotNetApis.SimpleInjector/ContainerExtensions.cs
@@ -17,93 +17,93 @@ namespace DotNetApis.SimpleInjector
             container.Register(() => new Lazy<Task<T>>(func));
         }
 
-        public static void RegisterSingletons<T1, T2>(this Container container, (T1, T2) tuple)
+        public static void RegisterInstances<T1, T2>(this Container container, (T1, T2) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
         }
 
-        public static void RegisterSingletons<T1, T2, T3>(this Container container, (T1, T2, T3) tuple)
+        public static void RegisterInstances<T1, T2, T3>(this Container container, (T1, T2, T3) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
         }
 
-        public static void RegisterSingletons<T1, T2, T3, T4>(this Container container, (T1, T2, T3, T4) tuple)
+        public static void RegisterInstances<T1, T2, T3, T4>(this Container container, (T1, T2, T3, T4) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
-            container.RegisterSingleton(typeof(T4), tuple.Item4);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T4), tuple.Item4);
         }
 
-        public static void RegisterSingletons<T1, T2, T3, T4, T5>(this Container container, (T1, T2, T3, T4, T5) tuple)
+        public static void RegisterInstances<T1, T2, T3, T4, T5>(this Container container, (T1, T2, T3, T4, T5) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
-            container.RegisterSingleton(typeof(T4), tuple.Item4);
-            container.RegisterSingleton(typeof(T5), tuple.Item5);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T4), tuple.Item4);
+            container.RegisterInstance(typeof(T5), tuple.Item5);
         }
 
-        public static void RegisterSingletons<T1, T2, T3, T4, T5, T6>(this Container container, (T1, T2, T3, T4, T5, T6) tuple)
+        public static void RegisterInstances<T1, T2, T3, T4, T5, T6>(this Container container, (T1, T2, T3, T4, T5, T6) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
-            container.RegisterSingleton(typeof(T4), tuple.Item4);
-            container.RegisterSingleton(typeof(T5), tuple.Item5);
-            container.RegisterSingleton(typeof(T6), tuple.Item6);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T4), tuple.Item4);
+            container.RegisterInstance(typeof(T5), tuple.Item5);
+            container.RegisterInstance(typeof(T6), tuple.Item6);
         }
 
-        public static void RegisterSingletons<T1, T2, T3, T4, T5, T6, T7>(this Container container, (T1, T2, T3, T4, T5, T6, T7) tuple)
+        public static void RegisterInstances<T1, T2, T3, T4, T5, T6, T7>(this Container container, (T1, T2, T3, T4, T5, T6, T7) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
-            container.RegisterSingleton(typeof(T4), tuple.Item4);
-            container.RegisterSingleton(typeof(T5), tuple.Item5);
-            container.RegisterSingleton(typeof(T6), tuple.Item6);
-            container.RegisterSingleton(typeof(T7), tuple.Item7);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T4), tuple.Item4);
+            container.RegisterInstance(typeof(T5), tuple.Item5);
+            container.RegisterInstance(typeof(T6), tuple.Item6);
+            container.RegisterInstance(typeof(T7), tuple.Item7);
         }
 
-        public static void RegisterSingletons<T1, T2, T3, T4, T5, T6, T7, T8>(this Container container, (T1, T2, T3, T4, T5, T6, T7, T8) tuple)
+        public static void RegisterInstances<T1, T2, T3, T4, T5, T6, T7, T8>(this Container container, (T1, T2, T3, T4, T5, T6, T7, T8) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
-            container.RegisterSingleton(typeof(T4), tuple.Item4);
-            container.RegisterSingleton(typeof(T5), tuple.Item5);
-            container.RegisterSingleton(typeof(T6), tuple.Item6);
-            container.RegisterSingleton(typeof(T7), tuple.Item7);
-            container.RegisterSingleton(typeof(T8), tuple.Item8);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T4), tuple.Item4);
+            container.RegisterInstance(typeof(T5), tuple.Item5);
+            container.RegisterInstance(typeof(T6), tuple.Item6);
+            container.RegisterInstance(typeof(T7), tuple.Item7);
+            container.RegisterInstance(typeof(T8), tuple.Item8);
         }
 
-        public static void RegisterSingletons<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this Container container, (T1, T2, T3, T4, T5, T6, T7, T8, T9) tuple)
+        public static void RegisterInstances<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this Container container, (T1, T2, T3, T4, T5, T6, T7, T8, T9) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
-            container.RegisterSingleton(typeof(T4), tuple.Item4);
-            container.RegisterSingleton(typeof(T5), tuple.Item5);
-            container.RegisterSingleton(typeof(T6), tuple.Item6);
-            container.RegisterSingleton(typeof(T7), tuple.Item7);
-            container.RegisterSingleton(typeof(T8), tuple.Item8);
-            container.RegisterSingleton(typeof(T9), tuple.Item9);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T4), tuple.Item4);
+            container.RegisterInstance(typeof(T5), tuple.Item5);
+            container.RegisterInstance(typeof(T6), tuple.Item6);
+            container.RegisterInstance(typeof(T7), tuple.Item7);
+            container.RegisterInstance(typeof(T8), tuple.Item8);
+            container.RegisterInstance(typeof(T9), tuple.Item9);
         }
 
-        public static void RegisterSingletons<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this Container container, (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) tuple)
+        public static void RegisterInstances<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this Container container, (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) tuple)
         {
-            container.RegisterSingleton(typeof(T1), tuple.Item1);
-            container.RegisterSingleton(typeof(T2), tuple.Item2);
-            container.RegisterSingleton(typeof(T3), tuple.Item3);
-            container.RegisterSingleton(typeof(T4), tuple.Item4);
-            container.RegisterSingleton(typeof(T5), tuple.Item5);
-            container.RegisterSingleton(typeof(T6), tuple.Item6);
-            container.RegisterSingleton(typeof(T7), tuple.Item7);
-            container.RegisterSingleton(typeof(T8), tuple.Item8);
-            container.RegisterSingleton(typeof(T10), tuple.Item10);
+            container.RegisterInstance(typeof(T1), tuple.Item1);
+            container.RegisterInstance(typeof(T2), tuple.Item2);
+            container.RegisterInstance(typeof(T3), tuple.Item3);
+            container.RegisterInstance(typeof(T4), tuple.Item4);
+            container.RegisterInstance(typeof(T5), tuple.Item5);
+            container.RegisterInstance(typeof(T6), tuple.Item6);
+            container.RegisterInstance(typeof(T7), tuple.Item7);
+            container.RegisterInstance(typeof(T8), tuple.Item8);
+            container.RegisterInstance(typeof(T10), tuple.Item10);
         }
     }
 }
