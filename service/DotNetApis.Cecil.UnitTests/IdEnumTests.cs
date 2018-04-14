7087abef80b31537db05cba5e276a60d1ad46a68
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-14 18:07:46 -0400

    Add matching struct and enum id unit tests.

---
+++ service/DotNetApis.Cecil.UnitTests/IdEnumTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static Utility;
+
+namespace DotNetApis.Cecil.UnitTests
+{
+    public class IdEnumTests
+    {
+        [Fact]
+        public void TopLevel()
+        {
+            var code = @"public enum SampleEnum { }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleEnum");
+            Assert.Equal("T:SampleEnum", type.XmldocIdentifier());
+            Assert.Equal("SampleEnum", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleEnum", name.SimpleName);
+            Assert.Equal("SampleEnum", name.QualifiedName);
+            Assert.Equal("SampleEnum", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void InSingleNamespace()
+        {
+            var code = @"namespace MyNamespace { public enum SampleEnum { } }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleEnum");
+            Assert.Equal("T:MyNamespace.SampleEnum", type.XmldocIdentifier());
+            Assert.Equal("MyNamespace.SampleEnum", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleEnum", name.SimpleName);
+            Assert.Equal("MyNamespace.SampleEnum", name.QualifiedName);
+            Assert.Equal("MyNamespace.SampleEnum", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void InMultipleNamespaces()
+        {
+            var code = @"namespace MyNamespace.InnerNamespace.ThirdNamespace { public enum SampleEnum { } }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleEnum");
+            Assert.Equal("T:MyNamespace.InnerNamespace.ThirdNamespace.SampleEnum", type.XmldocIdentifier());
+            Assert.Equal("MyNamespace.InnerNamespace.ThirdNamespace.SampleEnum", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleEnum", name.SimpleName);
+            Assert.Equal("MyNamespace.InnerNamespace.ThirdNamespace.SampleEnum", name.QualifiedName);
+            Assert.Equal("MyNamespace.InnerNamespace.ThirdNamespace.SampleEnum", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested()
+        {
+            var code = @"public class OuterClass { public enum SampleEnum { } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterClass");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleEnum");
+            Assert.Equal("T:OuterClass.SampleEnum", type.XmldocIdentifier());
+            Assert.Equal("OuterClass/SampleEnum", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterClass.SampleEnum", name.SimpleName);
+            Assert.Equal("OuterClass.SampleEnum", name.QualifiedName);
+            Assert.Equal("OuterClass.SampleEnum", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_InNamespace()
+        {
+            var code = @"namespace Ns { public class OuterClass { public enum SampleEnum { } } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterClass");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleEnum");
+            Assert.Equal("T:Ns.OuterClass.SampleEnum", type.XmldocIdentifier());
+            Assert.Equal("Ns.OuterClass/SampleEnum", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterClass.SampleEnum", name.SimpleName);
+            Assert.Equal("Ns.OuterClass.SampleEnum", name.QualifiedName);
+            Assert.Equal("Ns.OuterClass.SampleEnum", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_GenericParameters()
+        {
+            var code = @"public struct OuterClass<TFirst, TSecond> { public enum SampleEnum { } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterClass`2");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleEnum");
+            Assert.Equal("T:OuterClass`2.SampleEnum", type.XmldocIdentifier());
+            Assert.Equal("OuterClass'2/SampleEnum", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterClass<TFirst,TSecond>.SampleEnum", name.SimpleName);
+            Assert.Equal("OuterClass<TFirst,TSecond>.SampleEnum", name.QualifiedName);
+            Assert.Equal("OuterClass<TFirst,TSecond>.SampleEnum", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_GenericParameters_InNamespace()
+        {
+            var code = @"namespace Ns { public struct OuterClass<TFirst, TSecond> { public enum SampleEnum { } } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterClass`2");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleEnum");
+            Assert.Equal("T:Ns.OuterClass`2.SampleEnum", type.XmldocIdentifier());
+            Assert.Equal("Ns.OuterClass'2/SampleEnum", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterClass<TFirst,TSecond>.SampleEnum", name.SimpleName);
+            Assert.Equal("Ns.OuterClass<TFirst,TSecond>.SampleEnum", name.QualifiedName);
+            Assert.Equal("Ns.OuterClass<TFirst,TSecond>.SampleEnum", name.FullyQualifiedName);
+        }
+    }
+}
---
+++ service/DotNetApis.Cecil.UnitTests/IdStructTests.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static Utility;
+
+namespace DotNetApis.Cecil.UnitTests
+{
+    public class IdStructTests
+    {
+        [Fact]
+        public void TopLevel()
+        {
+            var code = @"public struct SampleStruct { }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleStruct");
+            Assert.Equal("T:SampleStruct", type.XmldocIdentifier());
+            Assert.Equal("SampleStruct", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleStruct", name.SimpleName);
+            Assert.Equal("SampleStruct", name.QualifiedName);
+            Assert.Equal("SampleStruct", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void InSingleNamespace()
+        {
+            var code = @"namespace MyNamespace { public struct SampleStruct { } }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleStruct");
+            Assert.Equal("T:MyNamespace.SampleStruct", type.XmldocIdentifier());
+            Assert.Equal("MyNamespace.SampleStruct", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleStruct", name.SimpleName);
+            Assert.Equal("MyNamespace.SampleStruct", name.QualifiedName);
+            Assert.Equal("MyNamespace.SampleStruct", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void InMultipleNamespaces()
+        {
+            var code = @"namespace MyNamespace.InnerNamespace.ThirdNamespace { public struct SampleStruct { } }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleStruct");
+            Assert.Equal("T:MyNamespace.InnerNamespace.ThirdNamespace.SampleStruct", type.XmldocIdentifier());
+            Assert.Equal("MyNamespace.InnerNamespace.ThirdNamespace.SampleStruct", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleStruct", name.SimpleName);
+            Assert.Equal("MyNamespace.InnerNamespace.ThirdNamespace.SampleStruct", name.QualifiedName);
+            Assert.Equal("MyNamespace.InnerNamespace.ThirdNamespace.SampleStruct", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested()
+        {
+            var code = @"public struct OuterStruct { public struct SampleStruct { } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterStruct");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleStruct");
+            Assert.Equal("T:OuterStruct.SampleStruct", type.XmldocIdentifier());
+            Assert.Equal("OuterStruct/SampleStruct", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterStruct.SampleStruct", name.SimpleName);
+            Assert.Equal("OuterStruct.SampleStruct", name.QualifiedName);
+            Assert.Equal("OuterStruct.SampleStruct", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_InNamespace()
+        {
+            var code = @"namespace Ns { public struct OuterStruct { public struct SampleStruct { } } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterStruct");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleStruct");
+            Assert.Equal("T:Ns.OuterStruct.SampleStruct", type.XmldocIdentifier());
+            Assert.Equal("Ns.OuterStruct/SampleStruct", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterStruct.SampleStruct", name.SimpleName);
+            Assert.Equal("Ns.OuterStruct.SampleStruct", name.QualifiedName);
+            Assert.Equal("Ns.OuterStruct.SampleStruct", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void SingleGenericParameter()
+        {
+            var code = @"public struct SampleStruct<TFirst> { }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleStruct`1");
+            Assert.Equal("T:SampleStruct`1", type.XmldocIdentifier());
+            Assert.Equal("SampleStruct'1", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleStruct<TFirst>", name.SimpleName);
+            Assert.Equal("SampleStruct<TFirst>", name.QualifiedName);
+            Assert.Equal("SampleStruct<TFirst>", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void MultipleGenericParameters()
+        {
+            var code = @"public struct SampleStruct<TFirst, TSecond> { }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleStruct`2");
+            Assert.Equal("T:SampleStruct`2", type.XmldocIdentifier());
+            Assert.Equal("SampleStruct'2", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("SampleStruct<TFirst,TSecond>", name.SimpleName);
+            Assert.Equal("SampleStruct<TFirst,TSecond>", name.QualifiedName);
+            Assert.Equal("SampleStruct<TFirst,TSecond>", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_GenericParameters()
+        {
+            var code = @"public struct OuterStruct<TFirst, TSecond> { public struct SampleStruct<TThird> { } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterStruct`2");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleStruct`1");
+            Assert.Equal("T:OuterStruct`2.SampleStruct`1", type.XmldocIdentifier());
+            Assert.Equal("OuterStruct'2/SampleStruct'1", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterStruct<TFirst,TSecond>.SampleStruct<TThird>", name.SimpleName);
+            Assert.Equal("OuterStruct<TFirst,TSecond>.SampleStruct<TThird>", name.QualifiedName);
+            Assert.Equal("OuterStruct<TFirst,TSecond>.SampleStruct<TThird>", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_GenericParameters_InNamespace()
+        {
+            var code = @"namespace Ns { public struct OuterStruct<TFirst, TSecond> { public struct SampleStruct<TThird> { } } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterStruct`2");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleStruct`1");
+            Assert.Equal("T:Ns.OuterStruct`2.SampleStruct`1", type.XmldocIdentifier());
+            Assert.Equal("Ns.OuterStruct'2/SampleStruct'1", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterStruct<TFirst,TSecond>.SampleStruct<TThird>", name.SimpleName);
+            Assert.Equal("Ns.OuterStruct<TFirst,TSecond>.SampleStruct<TThird>", name.QualifiedName);
+            Assert.Equal("Ns.OuterStruct<TFirst,TSecond>.SampleStruct<TThird>", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_GenericParametersOnlyOnOuter()
+        {
+            var code = @"public struct OuterStruct<TFirst, TSecond> { public struct SampleStruct { } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterStruct`2");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleStruct");
+            Assert.Equal("T:OuterStruct`2.SampleStruct", type.XmldocIdentifier());
+            Assert.Equal("OuterStruct'2/SampleStruct", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterStruct<TFirst,TSecond>.SampleStruct", name.SimpleName);
+            Assert.Equal("OuterStruct<TFirst,TSecond>.SampleStruct", name.QualifiedName);
+            Assert.Equal("OuterStruct<TFirst,TSecond>.SampleStruct", name.FullyQualifiedName);
+        }
+
+        [Fact]
+        public void Nested_GenericParameters_OnlyOnInner()
+        {
+            var code = @"public struct OuterStruct { public struct SampleStruct<TThird> { } }";
+            var assembly = Compile(code).Dll;
+            var outer = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "OuterStruct");
+            var type = outer.NestedTypes.Single(x => x.Name == "SampleStruct`1");
+            Assert.Equal("T:OuterStruct.SampleStruct`1", type.XmldocIdentifier());
+            Assert.Equal("OuterStruct/SampleStruct'1", type.DnaId());
+            var name = type.MemberFriendlyName();
+            Assert.Equal("OuterStruct.SampleStruct<TThird>", name.SimpleName);
+            Assert.Equal("OuterStruct.SampleStruct<TThird>", name.QualifiedName);
+            Assert.Equal("OuterStruct.SampleStruct<TThird>", name.FullyQualifiedName);
+        }
+    }
+}
