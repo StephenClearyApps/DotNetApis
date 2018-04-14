6b287a62022db5800453169893cb82a6bc5d1ae3
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-13 21:50:10 -0400

    Start on some (very long overdue) unit tests.

---
+++ service/DotNetApis.Cecil.UnitTests/CecilExtensions.IsExposed.cs
@@ -0,0 +1,20 @@
+﻿using DotNetApis.Cecil;
+using System;
+using System.Linq;
+using Xunit;
+using static Utility;
+
+namespace DotNetApis.Cecil.UnitTests
+{
+    public class IsExposedTests
+    {
+        [Fact]
+        public void PublicClass_IsExposed()
+        {
+            var code = @"public class SampleClass { }";
+            var assembly = Compile(code).Dll;
+            var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleClass");
+            Assert.True(type.IsExposed());
+        }
+    }
+}
---
+++ service/DotNetApis.Cecil.UnitTests/Utility.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+public static class Utility
+{
+    /// <summary>
+    /// Compiles a C# file from a string into an assembly in-memory (with xmldocs) and then parses it with Cecil (and XDocument).
+    /// </summary>
+    /// <param name="code"></param>
+    /// <returns></returns>
+    public static (AssemblyDefinition Dll, XDocument Xml) Compile(string code)
+    {
+        var parseOptions = new CSharpParseOptions()
+            .WithKind(SourceCodeKind.Regular)
+            .WithLanguageVersion(LanguageVersion.Latest);
+        var tree = CSharpSyntaxTree.ParseText(code, parseOptions);
+        var compileOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+            .WithOptimizationLevel(OptimizationLevel.Release);
+        var compilation = CSharpCompilation.Create("TestInMemoryAssembly", options: compileOptions)
+            .AddSyntaxTrees(tree)
+            .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+        var peStream = new MemoryStream();
+        var xmlStream = new MemoryStream();
+        var emitResult = compilation.Emit(peStream, xmlDocumentationStream: xmlStream);
+        if (!emitResult.Success)
+            throw new InvalidOperationException("Compilation failed: " + string.Join("\n", emitResult.Diagnostics));
+        peStream.Seek(0, SeekOrigin.Begin);
+        xmlStream.Seek(0, SeekOrigin.Begin);
+        return (AssemblyDefinition.ReadAssembly(peStream), XDocument.Load(xmlStream));
+    }
+}
