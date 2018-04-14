c8abe38a30235c514e9f133d2fd4f8cd147a6f50
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-14 17:59:19 -0400

    Simplified test.

--- service/DotNetApis.Logic.UnitTests/FormattingMethods.cs
+++ service/DotNetApis.Logic.UnitTests/FormattingMethods.cs
@@ -1,4 +1,5 @@
 ﻿using DotNetApis.Structure.Entities;
+using DotNetApis.Structure.TypeReferences;
 using System;
 using System.Linq;
 using Xunit;
@@ -19,21 +20,19 @@ namespace DotNetApis.Logic.UnitTests
                             public void SimpleMethod() { }
                         }";
             var structure = Format(code);
-            Assert.Collection(structure.Types,
-                t =>
-                {
-                    var type = (TypeEntity)t;
-                    Assert.Collection(type.Members.Instance, method =>
-                    {
-                        Assert.Equal(EntityKind.Method, method.Kind);
-                        Assert.IsType<MethodEntity>(method);
-                        Assert.Equal(EntityAccessibility.Public, method.Accessibility);
-                        Assert.Equal(EntityModifiers.None, method.Modifiers);
-                        Assert.Equal("SimpleMethod", method.Name);
-                        Assert.Equal("SampleClass/SimpleMethod()", method.DnaId);
-                        Assert.Contains("This is my method. It does something.", method.Xmldoc.Basic.ToXmlString());
-                    });
-                });
+            var type = structure.Types.OfType<TypeEntity>().Single(x => x.Name == "SampleClass");
+            var method = type.Members.Instance.OfType<MethodEntity>().Single(x => x.Name == "SimpleMethod");
+
+            // The method
+            Assert.Equal(EntityKind.Method, method.Kind);
+            Assert.Equal(EntityAccessibility.Public, method.Accessibility);
+            Assert.Equal(EntityModifiers.None, method.Modifiers);
+            Assert.Equal("SampleClass/SimpleMethod()", method.DnaId);
+            Assert.Contains("This is my method. It does something.", method.Xmldoc.Basic.ToXmlString());
+
+            // Return type
+            var returnType = (KeywordTypeReference)method.ReturnType;
+            Assert.Equal("void", returnType.Name);
         }
     }
 }
