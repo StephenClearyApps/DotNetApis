45523ffe5076fb1b90227a70b6e69c1efb7ba087
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-22 06:39:55 -0400

    Update withLoadPackageLogOnDemand to use Hoc.

--- web/src/components/hoc/withLoadPackageLogOnDemand.tsx
+++ web/src/components/hoc/withLoadPackageLogOnDemand.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 
-import { createLoadOnDemand, PackageLogRequestInjectedProps } from '.';
+import { createLoadOnDemand, PackageLogRequestInjectedProps, Hoc, PassthroughHoc } from '.';
 import { State } from "../../reducers/index";
 import { Actions } from "../../actions";
 
-/** Takes the package request props and loads the package */
-export const withLoadPackageLogOnDemand =
-<TProps extends {}>(Component: React.ComponentType<TProps & State & Actions & PackageLogRequestInjectedProps>) =>
-createLoadOnDemand<TProps & State & Actions & PackageLogRequestInjectedProps>({
-    hasStarted: props => !!props.pkgLogRequestStatus,
-    load: props => props.DocActions.getDocLog(props.pkgRequestStatus.normalizedPackageKey, props.pkgStatus.logUri)
-})(Component);
+type LoadPackageLogOnDemandRequiredProps = State & Actions & PackageLogRequestInjectedProps;
+
+function createLoadPackageLogOnDemand<TProps>(): Hoc<TProps & LoadPackageLogOnDemandRequiredProps> {
+    return createLoadOnDemand<TProps & LoadPackageLogOnDemandRequiredProps>({
+        hasStarted: props => !!props.pkgLogRequestStatus,
+        load: props => props.DocActions.getDocLog(props.pkgRequestStatus.normalizedPackageKey, props.pkgStatus.logUri)
+    });
+}
+
+/** Takes the package log request props and loads the package log */
+export const withLoadPackageLogOnDemand : PassthroughHoc<LoadPackageLogOnDemandRequiredProps> = createLoadPackageLogOnDemand();
