0b67f75b6506be39c21649d3e5edc02eb7d4975c
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-21 21:23:27 -0400

    Add Hoc types and convert createMaybe.

--- web/src/components/hoc/createMaybe.tsx
+++ web/src/components/hoc/createMaybe.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import { Hoc } from '.';
 
 /** Displays a component only while a given predicate returns true */
-export const createMaybe =
-    <TProps extends {}>(predicate: (props: TProps) => boolean) =>
-    (Component: React.ComponentType<TProps>) =>
-    (props: TProps) => predicate(props) ? <Component {...props}/> : null;
+export function createMaybe<TProps>(predicate: (props: TProps) => boolean): Hoc<TProps> {
+    return (Component: React.ComponentType<TProps>) =>
+        (props: TProps) => predicate(props) ? <Component {...props}/> : null;
+}
