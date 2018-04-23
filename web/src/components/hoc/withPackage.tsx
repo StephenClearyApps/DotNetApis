465bee11600a51040deb488ed35e8fab90ec7d40
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-22 20:57:08 -0400

    Change withPackaget to use Hoc.

--- web/src/components/hoc/withPackage.tsx
+++ web/src/components/hoc/withPackage.tsx
@@ -1,29 +1,33 @@
 import * as React from "react";
 
+import { Hoc, ExtendingHoc } from ".";
 import { PackageLoading } from "../PackageLoading";
 import { PackageRequestError } from '../PackageRequestError';
 
-import { PackageContextInjectedProps, withPackageContext, createLoadOnDemand, PackageRequestInjectedProps, createEither, withLoadPackageOnDemand, withPackageRequest, createRouterProps } from '.';
+import { PackageContextInjectedProps, withPackageContext, PackageRequestInjectedProps, createEither, withLoadPackageOnDemand, withPackageRequest, createRouterProps } from '.';
 import { State } from "../../reducers/index";
-import { Actions } from "../../actions";
 
 export type PackageInjectedProps = PackageContextInjectedProps;
 
-export const withPackage =
-<TProps extends {}>(Component: React.ComponentType<TProps & PackageInjectedProps>) => {
-    const withEitherLoadingMessage = createEither<TProps & State & PackageRequestInjectedProps>(
-        props => props.pkgRequestStatus && props.pkgRequestStatus.status !== 'STARTED',
-        props => <PackageLoading {...props}/>
-    );
-    const withEitherErrorMessage = createEither<TProps & State & PackageRequestInjectedProps>(
-        props => props.pkgRequestStatus.status !== 'ERROR' && props.pkgRequestStatus.status !== 'BACKEND_ERROR',
-        props => <PackageRequestError {...props}/>
-    );
-
-    return createRouterProps<PackageKey>()(
-        withPackageRequest(
-            withLoadPackageOnDemand(
-                withEitherLoadingMessage(
-                    withEitherErrorMessage(
-                        withPackageContext(Component))))));
+function createWithPackage<TProps>(): Hoc<TProps, TProps & PackageInjectedProps> {
+    return Component => {
+        const withEitherLoadingMessage = createEither<TProps & State & PackageRequestInjectedProps>(
+            props => props.pkgRequestStatus && props.pkgRequestStatus.status !== 'STARTED',
+            props => <PackageLoading {...props}/>
+        );
+        const withEitherErrorMessage = createEither<TProps & State & PackageRequestInjectedProps>(
+            props => props.pkgRequestStatus.status !== 'ERROR' && props.pkgRequestStatus.status !== 'BACKEND_ERROR',
+            props => <PackageRequestError {...props}/>
+        );
+    
+        return createRouterProps<PackageKey>()(
+            withPackageRequest(
+                withLoadPackageOnDemand(
+                    withEitherLoadingMessage(
+                        withEitherErrorMessage(
+                            withPackageContext(Component))))));
+    };
 }
+
+/** Determines the current package from the router, and injects the package context */
+export const withPackage : ExtendingHoc<PackageInjectedProps> = createWithPackage();
