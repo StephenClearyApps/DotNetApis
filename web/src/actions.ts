a1bb93ebc01377c8085784678ab6127a3fdced8a
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-04-28 21:13:56 -0400

    Simplify actions.

--- web/src/actions.ts
+++ web/src/actions.ts
@@ -1,4 +1,11 @@
-import * as a from './actions/index';
-export const actions = a;
+import { actions as TimeActions } from './ducks/time';
+import { actions as PackageLogActions } from './ducks/packageLog';
+import { actions as PackageDocActions } from './ducks/packageDoc';
+
+export const actions = {
+    TimeActions,
+    PackageLogActions,
+    PackageDocActions
+};
 
 export type Actions = typeof actions;
--- web/src/actions/index.ts
+++
@@ -1,3 +0,0 @@
-export { actions as TimeActions } from '../ducks/time';
-export { actions as PackageLogActions } from '../ducks/packageLog';
-export { actions as PackageDocActions } from '../ducks/packageDoc';
