cff8ca7bd90e93e4368bb3ea213878358ff2af60
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-05-02 21:28:48 -0400

    Minimize `any`.

--- web/src/store.ts
+++ web/src/store.ts
@@ -3,10 +3,10 @@ import immutableState from 'redux-immutable-state-invariant';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import { reducers } from './reducers';
+import { reducers, State } from './reducers';
 
 const middlewareEnhancer = process.env.NODE_ENV !== 'production' ? applyMiddleware(immutableState(), thunk) : applyMiddleware(thunk);
 const storeEnhancer = composeWithDevTools(middlewareEnhancer);
 
-const factory = storeEnhancer(createStore);
-export const store = factory(reducers as any);
+const factory = storeEnhancer<State>(createStore);
+export const store = factory(reducers);
